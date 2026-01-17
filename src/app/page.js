7be57413d9a95f21d2c5332c2f578b0f4a153ad5
Jean-Sebastien <jsruest123@gmail.com>
2026-01-16 23:34:15 -0500

    Initial MyManga setup

---
+++ src/app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-black text-white">
+      <h1 className="text-5xl font-bold">MyManga 📚</h1>
+    </main>
+  );
+}
